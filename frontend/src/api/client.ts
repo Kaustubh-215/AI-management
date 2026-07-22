fbfc63165c5d73426bfb96e8e045a4d4cb9dbc95
Kaustubh <pandekaustubh277@gmail.com>
2026-07-22 04:09:46 +0000

    Move deployment to Docker Hub

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://54.185.142.173.nip.io/api/v1",
+  baseURL: "https://54.68.149.130.nip.io/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
